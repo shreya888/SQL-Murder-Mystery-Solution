f82ec248f06703f61c929da2dd5124974dfabbb5
Shreya Chawla <shreyachawla2398@gmail.com>
2025-09-05 17:27:39 +1000

    Create 02_witness_statements.sql

---
+++ 02_witness_statements.sql
@@ -0,0 +1,21 @@
+/*
+ * File: 02_witness_statements.sql
+ * Purpose: Retrieve statements of witness 1 and witness 2 from the interview table.
+ * Context: Based on address and name info from the person table to identify witnesses.
+ */
+
+SELECT * 
+FROM interview
+WHERE person_id = (
+    SELECT id 
+    FROM person 
+    WHERE address_street_name = 'Northwestern Dr' 
+    ORDER BY address_number DESC 
+    LIMIT 1
+)
+OR person_id = (
+    SELECT id 
+    FROM person 
+    WHERE address_street_name = 'Franklin Ave' 
+      AND name LIKE '%Annabel%'
+);
