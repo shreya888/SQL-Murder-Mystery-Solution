47bff7253f7c10d51ddebfbb9731057614f96dce
Shreya Chawla <shreyachawla2398@gmail.com>
2025-09-15 01:37:58 +1000

    Create 05_culprit_interview.sql

---
+++ 05_culprit_interview.sql
@@ -0,0 +1,8 @@
+/*
+ * File: 05_culprit_interview.sql
+ * Purpose: Retrieve the interview record of the main suspect (person_id = '67318').
+ */
+
+SELECT * 
+FROM interview
+WHERE person_id = '67318';
